cc42a602f515500c382259a72b9d075c336b80c1
cl2032 <cl2032@pleasantonusd.net>
2018-02-12 22:52:02 -0800

    2121051

---
+++ Game.java
@@ -0,0 +1,185 @@
+import java.util.*;
+/**
+ * Write a description of class Interface here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Game
+{
+    private ArrayList <Integer> myArray;
+    private String listType;
+    private TicTacToe obj1 = new TicTacToe();
+
+    // instance variables - replace the example below with your own
+
+    public void newGame()
+    {
+        int noMoves = 0 ; int row = 0 ; int col = 0 ; 
+        Scanner input = new Scanner(System.in);
+        while( true ) 
+        {
+
+            System.out.print("Player X, which row?");
+            row = input.nextInt();
+            System.out.print("Player X, which col?");
+            col = input.nextInt();
+
+            while( !obj1.changeToX(row,col)   )
+            {
+                obj1.printBoard();
+                System.out.print("Player X, which row?");
+                row = input.nextInt();
+                System.out.print("Player X, which col?");
+                col = input.nextInt();    
+
+            }
+            obj1.printBoard();
+            if( obj1.findWin() == true )
+            {
+                System.out.println( "Player X is the winner");
+                break;
+
+            }
+            noMoves++ ;
+
+            if( noMoves == 9 ) 
+            {
+                //draw
+                System.out.println("It is a cats game.");
+                //break
+                break; 
+
+            }
+
+            System.out.print("Player Y, which row?");
+            row = input.nextInt();
+            System.out.print("Player Y, which col?");
+            col = input.nextInt();
+
+            while( !obj1.changeToO(row,col)   )
+            {
+                obj1.printBoard();
+                System.out.print("Player Y, which row?");
+                row = input.nextInt();
+                System.out.print("Player Y, which col?");
+                col = input.nextInt();   
+
+            }
+            obj1.printBoard();
+            if( obj1.findWin() == true )
+            {
+                System.out.println( "Player O is the winner");
+                break;
+            }
+            noMoves++ ;
+
+        }
+    }
+
+    public void newGamePlayerVsComputer()
+    {
+        int noMoves = 0 ; int row = 0 ; int col = 0 ; 
+        Scanner input = new Scanner(System.in);
+        while( true ) 
+        {
+
+            System.out.print("Player X, which row?");
+            row = input.nextInt();
+            System.out.print("Player X, which col?");
+            col = input.nextInt();
+            if ( row < 1 || row > 3 || col < 1 || col > 3 )
+            {
+                //System.out.println( "Square is out of bound" );   
+                
+            }
+            while( !obj1.changeToX(row,col)   )
+            {
+                obj1.printBoard();
+                System.out.print("Player X, which row?");
+                row = input.nextInt();
+                System.out.print("Player X, which col?");
+                col = input.nextInt();    
+
+            }
+            obj1.printBoard();
+            if( obj1.findWin() == true )
+            {
+                System.out.println( "Player X is the winner");
+                break;
+
+            }
+            noMoves++ ;
+
+            if( noMoves == 9 ) 
+            {
+                //draw
+                System.out.println("It is a cats game.");
+                //break
+                break; 
+
+            }
+
+            row = (int)( Math.random() *3+1);
+            col = (int)( Math.random() *3+1);
+            System.out.println( row + " : " + col ) ; 
+
+            while( !obj1.changeToO(row,col)   )
+            {
+                obj1.printBoard();
+                //System.out.print("Player Y, which row?");
+                row = (int)( Math.random() *3+1);
+                //System.out.print("Player Y, which col?");
+                col = (int)( Math.random() *3+1);  
+                System.out.println( row + " : " + col ) ; 
+                //try {
+                //System.in.read( ) ; }
+                //catch( Exception ex ) {}
+            }
+            obj1.printBoard();
+            if( obj1.findWin() == true )
+            {
+                System.out.println( "Computer is the winner");
+                break;
+            }
+            noMoves++ ;
+
+        }
+    }
+
+    public static void main(String args[])
+    {
+
+        Scanner input = new Scanner(System.in);
+        Game gameObj = new Game();
+        /*
+        System.out.println(" (1) player vs Player" );
+        System.out.println(" (2) player vs Computer" );
+        int option1 = input.nextInt() ; 
+        input.nextLine();  */
+        String str1 ; 
+        while( true ) 
+        {
+
+            System.out.println(" (1) player vs Player" );
+            System.out.println(" (2) player vs Computer" );
+            int option1 = input.nextInt() ; 
+            input.nextLine();  
+            gameObj.obj1.setBoard();
+
+            if( option1 == 1 ) 
+                gameObj.newGame();
+            else
+                gameObj.newGamePlayerVsComputer() ;
+            gameObj.obj1.printBoard();
+            System.out.println( "Play Again (Y/N):" ) ;
+            str1 = input.nextLine() ;
+            if ( str1.equals("n") || str1.equals("N") )
+            {
+                break ;
+            }
+
+        }
+    }
+}
+
---
+++ TicTacToe.java
@@ -0,0 +1,191 @@
+import java.util.*;
+/**
+ * Write a description of class TicTacToe here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TicTacToe
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private String tokenWinner = "";
+    static String[][] board = new String[3][3];
+    /**
+     * Constructor for objects of class TicTacToe
+     */
+    public static void setBoard()
+    {
+        // initialise instance variables
+        System.out.println( "       col col col " );
+        System.out.println( "        1   2   3  " );
+        System.out.println( "      -------------" );
+
+        for( int i1 = 0 ; i1 <3 ; i1++)
+        {
+            System.out.print( "Row " + (i1 +1 ) + " " );
+            for ( int i2 = 0 ; i2 < 3 ; i2++)
+            {
+                board[i1][i2] = ("   ");
+                System.out.print("|");
+                System.out.print(board[i1][i2]);
+            }
+            System.out.print("|");
+            System.out.println();
+            System.out.println("      -------------");
+        }
+    }
+
+    public static void printBoard()
+    {
+        // initialise instance variables
+
+        System.out.println( "       col col col " );
+        System.out.println( "        1   2   3  " );
+        System.out.println( "      -------------" );
+
+        for( int i1 = 0 ; i1 <3 ; i1++)
+        {
+            System.out.print( "Row " + (i1 +1 ) + " " );
+            for ( int i2 = 0 ; i2 < 3 ; i2++)
+            {
+                System.out.print("|");
+                System.out.print(board[i1][i2]);
+            }
+            System.out.print("|");
+            System.out.println();
+            System.out.println("      -------------");
+        }
+    }
+
+    public boolean changeToX(int row, int col)
+    {
+        if (board[row-1][col-1] == "   ")
+        {
+            board[row-1][col-1] = " X ";
+            return true ;    
+        }
+        else
+        {
+            System.out.println("Already played, try again");
+            return false ; 
+        }
+
+    }
+
+    public boolean changeToO(int row, int col)
+    {
+        if (board[row-1][col-1] == "   ")
+        {
+            board[row-1][col-1] = " O ";
+            return true;
+        }
+        else
+        {
+            System.out.println("Already played, try again");
+            return false;
+        }
+    }
+
+    public boolean checkToken( String token ) 
+    {
+        if( board[0][0] == token)
+        {
+            if( board[0][1] ==  token && board[0][2] == token )
+            {
+                return true ; 
+            }
+
+            if( board[1][1] ==  token && board[2][2] == token )
+            {
+                return true ; 
+            }
+
+            if( board[1][0] ==  token && board[2][0] == token )
+            {
+                return true ; 
+            }
+
+        }
+        if( board[0][1] ==  token)
+        {
+            if( board[1][1] ==  token && board[2][1] == token )
+            {
+                return true ; 
+            }
+
+        }
+
+        if( board[0][2] ==  token)
+        {
+            if( board[1][1] ==  token && board[2][0] == token )
+            {
+                return true ; 
+            }
+
+            if( board[1][2] ==  token && board[2][2] == token )
+            {
+                return true ; 
+            }
+
+        }
+
+        if( board[1][0] ==  token)
+        {
+            if( board[1][1] ==  token && board[1][2] == token )
+            {
+                return true ; 
+            }
+
+        }
+
+        if( board[2][0] ==  token)
+        {
+            if( board[2][1] ==  token && board[2][2] == token )
+            {
+                return true ; 
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public  boolean findWin()
+    {
+        if ( checkToken( " O " ) ) 
+        {
+            tokenWinner = "O" ;
+            return true ; 
+        }
+
+        if ( checkToken( " X " ) ) 
+        {
+            tokenWinner = "X" ;
+            return true ; 
+        }
+
+        return false ;
+    }
+
+    public static void main(String args[])
+    {
+        TicTacToe obj1 = new TicTacToe();
+        obj1.setBoard();
+        obj1.changeToO(2,2);
+        obj1.printBoard();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
